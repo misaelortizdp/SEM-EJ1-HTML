c2c08c4ba14af671725b09a66928e38543d152aa
misaelortizdp <misaelortiz58@gmail.com>
2022-05-04 16:29:02 -0500

    .

---
+++ js/code.js
@@ -0,0 +1,11 @@
+
+const over = ()=> {
+
+    const node = document.createElement('img');
+
+
+    document.getElementById('nav').innerHTML= '<img id="hover-desktop" src="../img/desktopHover.svg" alt="share">';
+    
+
+}
+
